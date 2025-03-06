54e8d11484717f8b009088ec0c8227e643cce9eb
Aleksandar Djokic <djokic_kg@hotmail.com>
2025-03-06 08:33:23 +0100

    Updated

--- files/already_confirmed.php
+++ files/already_confirmed.php
@@ -11,10 +11,9 @@
 	<p style = 'font-weight: bold;'>Look’s like you’re really on the ball. <?php echo $poRecord->tranId ?> </br> has already been confirmed on <?php echo substr($confirmedBy, strpos($confirmedBy, 'at') + 3); ?>.</p>
 	<p>If you would like to notify us of any changes please reach out to one</br> of the following departments to address:</p>
  
-<p>Planning: planing@ctg.us</br>
+<p>Planning: kristin@ctg.us</br>
 Logistics: logistics@ctg.us</br>
-Finance: finance@ctg.us</br>
-</p>
+Finance: billing@ctg.us</p> 
  
 <p>Thank you.</p>
 </body>
